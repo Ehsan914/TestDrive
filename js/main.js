95999d108a59046b214148c451933a242d690f3b
Ehsan914 <ehsanhq963@gmail.com>
2024-09-09 00:47:49 +0600

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+// const p = document.getElementById('text');
+// let text = `<a href='#'><p>Testing...`;
+
+// for (let i = 0; i < 10; i++) {
+//     text += `.`;
+// }
+
+// text += `</p></a>`;
+
+// p.innerHTML = Date();
+
+// setTimeout(function() {
+//     p.style.fontSize = `50px`;
+//     console.log(p.style.fontSize);
+// }, 2000)
+
+// function myMove() {
+//     const animate = document.getElementById('animate');
+//     let pos = 0;
+
+//     const move = setInterval(() => {
+//         animate.style.top = pos + 'px';
+//         animate.style.left = pos + 'px';
+//         if (pos > 548) {
+//             clearInterval(move);
+//         }
+//         pos += 2;
+//     }, 5);
+// }
+
+const display = document.getElementById('text');
+
+function getPosition() {
+    if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(showPosition);
+    }
+}
+
+function showPosition(position) {
+    display.innerHTML = "Latitude: " + position.coords.latitude +
+                        "<br/>Longitude: " + position.coords.longitude;
+}
